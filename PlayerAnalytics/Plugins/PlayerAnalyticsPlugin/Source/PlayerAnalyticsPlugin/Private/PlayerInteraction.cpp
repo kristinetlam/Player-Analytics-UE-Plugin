30eaae24c3eb89430018ca5a9a5b2f78771169a6
Jessica Jakubik <123000032+jessjakubik@users.noreply.github.com>
2025-03-04 16:43:32 -0600

    Log Health Change
    currently can't call Start Position Tracking or Log Player Health Change on my end, checking if someone else can call them on their end

--- PlayerAnalytics/Plugins/PlayerAnalyticsPlugin/Source/PlayerAnalyticsPlugin/Private/PlayerInteraction.cpp
+++ PlayerAnalytics/Plugins/PlayerAnalyticsPlugin/Source/PlayerAnalyticsPlugin/Private/PlayerInteraction.cpp
@@ -28,3 +28,26 @@ void UPlayerInteraction::LogPlayerInteraction(AActor* InteractingActor, FString 
     //     GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, LogMessage);
     // }
 }
+
+void UPlayerInteraction::LogPlayerHealthChange(AActor* InteractingActor, FString InteractionDescription, int health)
+{
+    //actor that is changing the health (Enemy, Bullet, Spikes on the ground, Health Potion, etc.)
+    if (!InteractingActor)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Interaction attempted with a null actor!"));
+        return;
+    }
+
+    // Get current timestamp
+    FDateTime Now = FDateTime::Now();
+    FString Timestamp = Now.ToString(TEXT("%Y-%m-%d %H:%M:%S"));
+
+    // Get the actor's name
+    FString ActorName = InteractingActor->GetName();
+
+    // Create log message with timestamp
+    FString LogMessage = FString::Printf(TEXT("[%s] %s interacted: %s"), *Timestamp, *ActorName, *InteractionDescription, health);
+
+    // Print to Unreal's output log
+    UE_LOG(LogTemp, Log, TEXT("%s"), *LogMessage);
+}
