697cc71e1dc3bca6afec1cbf18afb7fae68d416e
kristinetlam <kristinetlam21@gmail.com>
2025-03-17 17:18:07 -0500

    testing vite config file

--- Frontend/Unreal-Frontend/vite.config.js
+++ Frontend/Unreal-Frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist', // Output directory for build files
     rollupOptions: {
-      input: path.resolve(__dirname, './index.html')
-    }
-  }
-});
+      input: 'index.html', // Specify the entry HTML file if needed
+    },
+  },
+})
