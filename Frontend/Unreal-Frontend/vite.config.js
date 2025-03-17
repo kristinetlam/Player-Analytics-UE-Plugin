4ba421b113dccc3434665cd1ea91f9ac094b6d87
kristinetlam <kristinetlam21@gmail.com>
2025-03-17 17:20:09 -0500

    testing vite config file 2

--- Frontend/Unreal-Frontend/vite.config.js
+++ Frontend/Unreal-Frontend/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.', // This ensures that Vite looks for index.html in the root folder
   plugins: [react()],
   build: {
-    outDir: 'dist', // Output directory for build files
+    outDir: 'dist',
     rollupOptions: {
-      input: 'index.html', // Specify the entry HTML file if needed
+      input: 'index.html', // Make sure Vite uses the correct HTML entry file
     },
   },
 })
