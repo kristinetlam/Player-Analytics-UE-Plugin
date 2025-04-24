378395e95c86a8ed793b56cbf912eeb87e5f0272
Jessica Jakubik <123000032+jessjakubik@users.noreply.github.com>
2025-04-24 14:49:11 -0500

    Revert "Styling fixes, more padding, equal heights"
    This reverts commit 435626965bcbecd4ea145c6da719926834bf70aa.

--- Frontend/Unreal-Frontend/src/components/dashboard/Dashboard.jsx
+++ Frontend/Unreal-Frontend/src/components/dashboard/Dashboard.jsx
@@ -228,19 +228,15 @@ function DashboardLayoutBasic() {
                 description="Measures return rates based on how many sessions a player has logged in for" centerContent><PlayerRetentionGraph filter={filter} /></CardComponent>
             {/* <CardComponent title="Item Usage" infoContent="sadsd" description="Displays the distribution of player item usage" pieBottom centerContent><BasicPie filter={filter} /></CardComponent> */}
             <CardComponent title="Player Session Statistics" infoContent="Displays summary statistics for all player session lengths within the selected time range. Includes the average (mean), median, and full range (shortest to longest session). If a player ID is selected, the stats reflect only that player's session data."
-                description="Summarizes player session data with key metrics"  sx={{ flex: '1 1 600px', maxWidth: '600px', minWidth: '400px', minHeight: '330px', maxHeight: '500px' }} centerContent><PlayerSessionStats filter={filter} /></CardComponent>
+                description="Summarizes player session data with key metrics"  sx={{ flex: '1 1 600px', maxWidth: '600px', minWidth: '400px', minHeight: '330px', maxHeight: '400px' }} centerContent><PlayerSessionStats filter={filter} /></CardComponent>
             <CardComponent title="FPS Performance Scatterplot" infoContent="Displays FPS values recorded per player session across time. Each dot represents a session's average FPS, helping visualize performance variations by date."
                 description="Tracks frame rate patterns across multiple players and dates" centerContent><FPSOverTime filter={filter} /></CardComponent>
             <CardComponent title="Average FPS Timeline" infoContent="Displays the average FPS per day aggregated across all player sessions. Useful for spotting trends or fluctuations in frame rate performance."
-                description="Player FPS averages grouped by day over time" sx={{ flex: '1 1 600px', maxWidth: '600px', minWidth: '400px', minHeight: '330px', maxHeight: '700px' }} centerContent><FPSLineChart filter={filter} /></CardComponent>
+                description="Player FPS averages grouped by day over time" sx={{ flex: '1 1 600px', maxWidth: '600px', minWidth: '400px', minHeight: '330px', maxHeight: '500px' }} centerContent><FPSLineChart filter={filter} /></CardComponent>
             <CardComponent title="Player Session Length" infoContent="Displays average session lengths over time, segmented by game version. Each line represents a different patch version and shows how long players stayed in-game on each day. This helps identify trends or anomalies related to specific updates."
                 description="Illustrates player session lengths grouped by game version patches" centerContent><AverageSessionPerDayChart filter={filter} /></CardComponent>
             <CardComponent title="Average Return Time" infoContent="Shows the average amount of time players take to return between each session, identifying player re-engagement speed or gaps in activity across consecutive logins."
-<<<<<<< Updated upstream
                description="Measures the average time between sessions" sx={{ flex: '1 1 300px', maxWidth: '360px', minWidth: '300px', maxHeight: '300px'}}  centerContent><AverageReturnTimeGraph filter={filter} /></CardComponent>
-=======
-               description="Measures the average time between sessions" sx={{ flex: '1 1 500px', maxWidth: '700px', minWidth: '300px', maxHeight: '700px', minHeight: '500px'}}  centerContent><AverageReturnTimeGraph filter={filter} /></CardComponent>
->>>>>>> Stashed changes
             <CardComponent title="Heatmap" description="Visualizes selected values averaged across the game map."  sx={{ flex: '1 1 600px', maxWidth: '700px', minWidth: '400px', minHeight: '380px'}} centerContent={false}><Heatmap filter={filter} /></CardComponent>
             <CardComponent title="Player Location" infoContent="Plots the X and Y coordinates of player positions during gameplay. This can help identify hotspots, player movement patterns, or underutilized areas of the map."
                 description="Visualizes player location across the game map" sx={{ minWidth:'700px'}} centerContent={true}><LocationScatterplot filter={filter} /></CardComponent>
