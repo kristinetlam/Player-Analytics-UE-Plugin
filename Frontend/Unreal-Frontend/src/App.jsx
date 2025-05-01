2335a0414505bd6f908f272397ae737a19e3406d
Jessica Jakubik <123000032+jessjakubik@users.noreply.github.com>
2025-05-01 18:51:26 -0500

    removed basic unused charts

--- Frontend/Unreal-Frontend/src/App.jsx
+++ Frontend/Unreal-Frontend/src/App.jsx
@@ -1,12 +1,6 @@
 import './App.css'
-import BasicBars from './components/graphs/PlayerInteractionGraph'
-import BasicLineChart from './components/graphs/LineGraph'
-import BasicPie from './components/graphs/PieChart'
-import BasicScatter from './components/graphs/ScatterPlot'
-import CardComponent from './components/CardComponent'
 // import Header from './components/header/Header'
 import DashboardLayoutBasic from './components/dashboard/Dashboard'
-import DoughnutChart from './components/graphs/DoughnutChart'
 
 function App() {
   return (
